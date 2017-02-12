f040a3e779809cac73d10df47e78ede227bb7d4b
Theo Lee <theodorelcj@gmail.com>
2017-02-12 22:29:21 +0800

    Update miscellaneous.js

--- note/miscellaneous.js
+++ note/miscellaneous.js
@@ -1,1 +1,10 @@
-
+function CheckSmallScreen() {
+  if (window.innerWidth == 540 || window.innerWidth < 540) {
+    document.getElementById("App-InviteCollabs").style.display = "none";
+    document.getElementById("App-Functions-Invite").style.display = "block";
+  }
+  else {
+    document.getElementById("App-InviteCollabs").style.display = "block";
+    document.getElementById("App-Functions-Invite").style.display = "none";
+  }
+}
