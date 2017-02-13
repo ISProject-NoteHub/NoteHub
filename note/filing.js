b8c457ae2fa51127d9158fc47f3f6248fa01fa76
Ho Wing Yip <sploooosh884@gmail.com>
2017-02-13 22:16:33 +0800

    Starting on filing.js Theodore, please help me do the parameters, then I'll do the aesthetics (e.g. creating new div to display "Note Saved!" message)

--- note/filing.js
+++ note/filing.js
@@ -1,1 +1,10 @@
-
+function savenote() {
+    var getphpfile = new XMLHttpRequest();
+    getphpfile.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert('Note Saved!')
+        }
+    }
+    getphpfile.open("GET", "https://notehub-serverside.000webhostapp.com/handlers/filing.php", true);
+    getphpfile.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+}
