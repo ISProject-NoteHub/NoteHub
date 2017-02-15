c4ab26163a4e3ee437e37e07dbef9b8cf3b32d21
Theo Lee <theodorelcj@gmail.com>
2017-02-15 16:09:21 +0800

    Update filing.js

--- note/filing.js
+++ note/filing.js
@@ -13,7 +13,7 @@ function GetNote(topic, noteID) {
   var getPHPFile = new XMLHttpRequest();
   getPHPFile.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("App-NoteBox").innerHTML = getPHPfile.responseText;
+      document.getElementById("App-NoteBox").innerHTML = getPHPFile.responseText;
     }
   }
   getPHPFile.open("POST", "https://notehub-backend.gear.host/handlers/filing.php", true);
