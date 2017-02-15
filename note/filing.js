12f92d6e1ed3166c0cbb1c25a9d62f01c2c50691
Theo Lee <theodorelcj@gmail.com>
2017-02-15 09:14:09 +0800

    Update filing.js

--- note/filing.js
+++ note/filing.js
@@ -1,10 +1,14 @@
-function savenote() {
-    var getphpfile = new XMLHttpRequest();
-    getphpfile.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert('Note Saved!')
-        }
+function SaveNote() {
+  var getPHPFile = new XMLHttpRequest();
+  getPHPFile.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      alert('Note Saved!')
     }
-    getphpfile.open("GET", "https://notehub-serverside.000webhostapp.com/handlers/filing.php", true);
-    getphpfile.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  }
+  getPHPFile.open("POST", "https://notehub-serverside.000webhostapp.com/handlers/filing.php", true);
+  getPHPFile.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+}
+
+function GetNote(topic, noteID) {
+  //Add parameters 'topic' and 'noteID'
 }
