a0c8b158ae1a2b2338190e04cabaade2dc19fecd
TheoLeeCJ <theodorelcj@gmail.com>
2017-03-18 03:11:05 +0800

    Updated filing.js

--- note/filing.js
+++ note/filing.js
@@ -16,6 +16,6 @@ function GetNote(topic, noteID) {
       document.getElementById("App-NoteBox").innerHTML = getPHPFile.responseText;
     }
   }
-  getPHPFile.open("POST", "https://notehub-backend.gear.host/handlers/filing.php?requestedFunction=GetNote&topic=getting-started&notename=getting-started-00001.txt", true);
+  getPHPFile.open("POST", "http://notehub-serverside.000webhostapp.com/handlers/filing.php?requestedFunction=read&topic=getting-started-with-notehub&noteId=notehub-000001-A.json", true);
   getPHPFile.send();
 }
