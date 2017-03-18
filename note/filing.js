b120d38127be5ff4730faa3ede59c226c4982260
Theo Lee <theodorelcj@gmail.com>
2017-03-18 19:45:49 +0800

    Update filing.js

--- note/filing.js
+++ note/filing.js
@@ -14,8 +14,11 @@ function GetNote(topic, noteID) {
   getPHPFile.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("App-NoteBox").innerHTML = getPHPFile.responseText;
+      
+      var noteReceived = JSON.parse(getPHPFile.responseText);
+      console.log(noteReceived);
     }
   }
-  getPHPFile.open("POST", "http://notehub-serverside.000webhostapp.com/handlers/filing.php?requestedFunction=read&topic=getting-started-with-notehub&noteId=notehub-000001-A.json", true);
+  getPHPFile.open("POST", "https://notehub-serverside.000webhostapp.com/handlers/filing.php?requestedFunction=read&topic=getting-started-with-notehub&noteId=notehub-000001-A.json", true);
   getPHPFile.send();
 }
