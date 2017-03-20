d9e09c435bd69568bdabcfc0cac4091399f93899
Theo Lee <theodorelcj@gmail.com>
2017-03-20 09:27:42 +0800

    Update miscellaneous.js

--- note/miscellaneous.js
+++ note/miscellaneous.js
@@ -1,3 +1,6 @@
+var notehubVersion = "20th March 2017, Build 1", localDataChangesMade = "No changes"; //Tracks for whether or not user's localStorage requires updates.  Can be 'No changes' or 'Changes made'.
+
+//Adapt layout for small screen
 function CheckSmallScreen() {
   if (window.innerWidth == 540 || window.innerWidth < 540) {
     document.getElementById("App-InviteCollabs").style.display = "none";
@@ -6,5 +9,25 @@ function CheckSmallScreen() {
   else {
     document.getElementById("App-InviteCollabs").style.display = "block";
     document.getElementById("App-Functions-Invite").style.display = "none";
+  }
+}
+
+//Check for first use
+function DoNecessaryForFirstUse() {
+  if (localStorage.getItem("newUser") == "nope") { return; }
+  else {
+    localStorage.setItem("newUser", "nope");
+    GetNote("notehub-basics", "notehub-000001-A.json");
+  }
+  
+  CheckVersion();
+}
+
+//Check for version of NoteHub that localStorage has recognised
+function CheckVersion() {
+  if (localStorage.getItem("notehubVersion") == notehubVersion) { return; }
+  else {
+    //Fix later
+    localStorage.setItem("notehubVersion", notehubVersion);
   }
 }
