233a465ce16efcbf517cc509e58c922e6191f811
Theo Lee <theodorelcj@gmail.com>
2017-04-14 00:37:57 +0800

    Update formatting.js

--- note/formatting.js
+++ note/formatting.js
@@ -12,7 +12,7 @@ function fonts_updateFont(event) {
   var spanToInsert = document.createElement("span");
   spanToInsert.innerHTML = text;
   spanToInsert.style.fontFamily = document.getElementById("Formatting-FontFamily").options[document.getElementById("Formatting-FontFamily").selectedIndex].value;
-  spanToInsert.style.fontSize = document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value;
+  spanToInsert.style.fontSize = document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value + "px";
   
   sel = window.getSelection();
   if (sel.getRangeAt && sel.rangeCount)
