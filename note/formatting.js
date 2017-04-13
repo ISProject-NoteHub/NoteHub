8d4309c31f9d873f4e3b78259cc5f155be8a4a8f
Theo Lee <theodorelcj@gmail.com>
2017-04-13 22:50:04 +0800

    Update formatting.js

--- note/formatting.js
+++ note/formatting.js
@@ -1,1 +1,14 @@
-
+//Fonts
+class fonts {
+  static setNewFont() {
+    alert("Setting new font.");
+  }
+  
+  static setNewSize() {
+    alert("Setting new font size.");
+  }
+  
+  static setBoldItalicUnderline() {
+    alert("Setting and checking for bolds, italics and underlines.");
+  }
+}
