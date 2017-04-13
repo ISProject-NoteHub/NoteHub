09e4aeb08ac1cbcd8d5d3a6989ddff7c85aa6ca4
Theo Lee <theodorelcj@gmail.com>
2017-04-14 01:54:49 +0800

    Update formatting.js

--- note/formatting.js
+++ note/formatting.js
@@ -1,6 +1,30 @@
 //Notebox
 var notebox, newSpanContents;
 
+//Bold
+function SetBold() {
+  var boldButton = document.getElementById("Formatting-FontBold");
+  
+  if (boldButton.getAttribute("data-active") == "true") { boldButton.setAttribute("data-active", "false"); }
+  else { boldButton.setAttribute("data-active", "true"); }
+}
+
+//Italic
+function SetItalic() {
+  var italicButton = document.getElementById("Formatting-FontItalic");
+  
+  if (italicButton.getAttribute("data-active") == "true") { italicButton.setAttribute("data-active", "false"); }
+  else { italicButton.setAttribute("data-active", "true"); }
+}
+
+//Underline
+function SetUnderline() {
+  var underlineButton = document.getElementById("Formatting-FontUnderline");
+  
+  if (underlineButton.getAttribute("data-active") == "true") { underlineButton.setAttribute("data-active", "false"); }
+  else { underlineButton.setAttribute("data-active", "true"); }
+}
+
 //Fonts
 function fonts_updateFont(event) {
   notebox = document.getElementById("App-NoteBox");
@@ -11,8 +35,15 @@ function fonts_updateFont(event) {
   text = window.getSelection().toString();
   var spanToInsert = document.createElement("span");
   spanToInsert.innerHTML = text;
+  
+  //Font size and font family
   spanToInsert.style.fontFamily = document.getElementById("Formatting-FontFamily").options[document.getElementById("Formatting-FontFamily").selectedIndex].value;
   spanToInsert.style.fontSize = document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value + "px";
+  
+  //Bold, italic, underline
+  if (document.getElementById("Formatting-FontUnderline").getAttribute("data-active") == "true") { spanToInsert.innerHTML = "<u>" + spanToInsert.innerHTML + "</u>"; }
+  if (document.getElementById("Formatting-FontBold").getAttribute("data-active") == "true") { spanToInsert.innerHTML = "<b>" + spanToInsert.innerHTML + "</b>"; }
+  if (document.getElementById("Formatting-FontItalic").getAttribute("data-active") == "true") { spanToInsert.innerHTML = "<i>" + spanToInsert.innerHTML + "</i>"; }
   
   sel = window.getSelection();
   if (sel.getRangeAt && sel.rangeCount)
