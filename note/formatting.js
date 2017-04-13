1e4d1a65debc3c9f9a11268ab2a6051e0ab86c49
Theo Lee <theodorelcj@gmail.com>
2017-04-14 00:22:23 +0800

    Update formatting.js

--- note/formatting.js
+++ note/formatting.js
@@ -1,12 +1,25 @@
+//Notebox
+var notebox = document.getElementById("App-NoteBox");
+var newSpanContents
+
 //Fonts
-function fonts_setNewFont() {
-  alert("Setting new font.");
-}
+function fonts_updateFont() {
+  notebox.focus();
+  notebox.removeAttribute("contenteditable");
 
-function fonts_setNewSize() {
-  alert("Setting new font size.");
-}
-
-function fonts_setBoldItalicUnderline() {
-  alert("Setting and checking for bolds, italics and underlines.");
+  var sel, range, html;
+  var text = "<table>";
+  text = window.getSelection().toString();
+  var spanToInsert = document.createElement("span");
+  spanToInsert.innerHTML = text;
+  spanToInsert.style.fontFamily = document.getElementById("Formatting-FontFamily").options[document.getElementById("Formatting-FontFamily").selectedIndex].value;
+  spanToInsert.style.fontSize = document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value;
+  
+  sel = window.getSelection();
+  if (sel.getRangeAt && sel.rangeCount)
+  {
+    range = sel.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(spanToInsert);
+  }
 }
