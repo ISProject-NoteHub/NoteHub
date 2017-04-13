d9730371731c457c8bbf5724898ed8f8a67d4bec
Theo Lee <theodorelcj@gmail.com>
2017-04-14 00:26:50 +0800

    Update formatting.js

--- note/formatting.js
+++ note/formatting.js
@@ -6,7 +6,6 @@ var newSpanContents;
 function fonts_updateFont() {
   notebox = document.getElementById("App-NoteBox");
   notebox.focus();
-  notebox.removeAttribute("contenteditable");
 
   var sel, range, html;
   var text = "<table>";
@@ -14,7 +13,7 @@ function fonts_updateFont() {
   var spanToInsert = document.createElement("span");
   spanToInsert.innerHTML = text;
   spanToInsert.style.fontFamily = document.getElementById("Formatting-FontFamily").options[document.getElementById("Formatting-FontFamily").selectedIndex].value;
-  spanToInsert.style.fontSize = document.getElementById("Formatting-FontSize").value;
+  spanToInsert.style.fontSize = document.getElementById("Formatting-FontSize").value + "px";
   
   sel = window.getSelection();
   if (sel.getRangeAt && sel.rangeCount)
