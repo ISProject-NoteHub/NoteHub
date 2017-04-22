a0cbe80c1aa05adda875fd15a10bddbf555dda53
Theo Lee <theodorelcj@gmail.com>
2017-04-23 00:52:36 +0800

    Update new-formatting.js

--- note/new-formatting.js
+++ note/new-formatting.js
@@ -14,5 +14,12 @@ function SetUnderline(event) {
 }
 
 function UpdateFontSize() {
-  document.execCommand("fontSize", false, document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value + "px");
+  document.execCommand("fontSize", false, "7");
+  var fontElements = document.getElementsByTagName("font");
+  for (var i = 0, len = fontElements.length; i < len; ++i) {
+    if (fontElements[i].size == "7") {
+      fontElements[i].removeAttribute("size");
+      fontElements[i].style.fontSize = document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value + "px" + "px";
+    }
+  }
 }
