97d64c5ba574150337875ffbf35ebcc3502bf21c
Theo Lee <theodorelcj@gmail.com>
2017-04-23 00:43:58 +0800

    Update new-formatting.js

--- note/new-formatting.js
+++ note/new-formatting.js
@@ -2,3 +2,17 @@ function SetBold(event) {
   event.preventDefault();
   document.execCommand("bold", false, null);
 }
+
+function SetItalic(event) {
+  event.preventDefault();
+  document.execCommand("italic", false, null);
+}
+
+function SetUnderline(event) {
+  event.preventDefault();
+  document.execCommand("underline", false, null);
+}
+
+function UpdateFontSize() {
+  document.execCommand("fontSize", false, document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value + "px");
+}
