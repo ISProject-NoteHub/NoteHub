e97fffd4ee8c26b76fc866579afdce31fffdc1e1
Theo Lee <theodorelcj@gmail.com>
2017-04-25 13:34:59 +0800

    Create new-miscellaneous.js

---
+++ note/new-miscellaneous.js
@@ -0,0 +1,6 @@
+function RevealIconDescriptors() {
+  var decription = document.getElementById("Menu-Descriptors");
+  
+  if (description.style.display == "block") { description.style.display = "none"; }
+  else { description.style.display = "none"; }
+}
