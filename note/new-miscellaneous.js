0af7a97ee6c385547f8da10b02e074de7ce62131
Theo Lee <theodorelcj@gmail.com>
2017-04-26 16:09:54 +0800

    Update new-miscellaneous.js

--- note/new-miscellaneous.js
+++ note/new-miscellaneous.js
@@ -4,3 +4,11 @@ function RevealIconDescriptors() {
   if (description.getAttribute("data-open") == "closed") { description.setAttribute("data-open", "open"); }
   else { description.setAttribute("data-open", "closed");  }
 }
+
+function Autorun() {
+  if (CheckSignIn() === true) { /*Nothing*/ }
+  else {
+    document.getElementById("MyAccount").style.display = "none";
+    document.getElementById("LoggedInOut").innerHTML = "Login";
+  }
+}
