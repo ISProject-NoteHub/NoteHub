311683181394a71425fb07f11085b82118c49459
TheoLeeCJ <theodorelcj@gmail.com>
2017-05-24 01:34:13 +0800

    Linking Viewer and Editor to Backend
    The viewer and editor have been linked (partially) to the backend.

---
+++ note/formatting.js
@@ -0,0 +1,32 @@
+function SetBold(event) {
+  event.preventDefault();
+  document.execCommand("bold", false, null);
+}
+
+function SetItalic(event) {
+  event.preventDefault();
+  document.execCommand("italic", false, null);
+}
+
+function SetUnderline(event) {
+  event.preventDefault();
+  document.execCommand("underline", false, null);
+}
+
+function UpdateFontSize() {
+  document.execCommand("fontSize", false, "7");
+  var fontElements = document.getElementsByTagName("font");
+  for (var i = 0, len = fontElements.length; i < len; ++i) {
+    if (fontElements[i].size == "7") {
+      fontElements[i].removeAttribute("size");
+      fontElements[i].style.fontSize = document.getElementById("Formatting-FontSize").options[document.getElementById("Formatting-FontSize").selectedIndex].value + "px";
+    }
+  }
+}
+
+function UpdateFontFamily() {
+  document.execCommand("fontName", false, document.getElementById("Formatting-FontFamily").options[document.getElementById("Formatting-FontFamily").selectedIndex].value);
+}
+
+function UpdateHighlight() {
+}
