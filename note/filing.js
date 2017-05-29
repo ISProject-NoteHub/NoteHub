fb54b83ec376565098d2fab5739c02e76c26f4bb
TheoLeeCJ <theodorelcj@gmail.com>
2017-05-29 19:38:33 +0800

    File System Part 1 of 3

--- note/filing.js
+++ note/filing.js
@@ -1,49 +1,149 @@
-var currentNoteAsObject = null;
-var privateNote = false;
+var currentNoteAsObject = null, noteList = null, privateNote = false, isSuggestion = false, noteOpened = false;
 
-//Save Note As
-function SaveNoteAs() {
-  //Here, check for conflicting naming in notebook
-  var noteAsJSON = JSON.stringify({
-    meta: {
-      name: document.getElementById("Title-Title").value,
-      topics: document.getElementById("Debug-Tags").value.split(","),
-      notebook: document.getElementById("Debug-Notebook").value, "comment": "All notes in this notebook will appear editable but only save under the user's account when saved, because they are tutorials.",
-      author: document.getElementById("Debug-Author").value
-    },
-    content: document.getElementById("Editor").innerHTML
-  });
-  
+//Initialise file picker
+function InitFilePicker() {
   var getPHPFile = new XMLHttpRequest();
   getPHPFile.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(decodeHTML(getPHPFile.responseText));
-      var response = JSON.parse(decodeHTML(getPHPFile.responseText));
-      console.log("Note saved.");
-
-      //If errors occured
-      if (response.errors == "notAuthenticated") {
-      }
-      else if (response.errors == "over9000") {
-      }
+      noteList = JSON.parse(getPHPFile.responseText);
+      ParseInTopics("Modal-SaveFilePicker");
+      ShowModal("SaveFilePicker");
     }
   }
-          
-  getPHPFile.open("POST", "https://notehub-serverside.000webhostapp.com/handlers/filing.php", true);
-  getPHPFile.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  getPHPFile.send("requestedFunction=write&checkConflict=true&note=" + noteAsJSON + "&noteId=" + document.getElementById("Title-Title").value + ".json" + "&topic=private-notes/" + document.getElementById("Debug-Author").value);
+  getPHPFile.open("GET", "https://notehub-serverside.000webhostapp.com/notes/notes-map.php");
+  getPHPFile.send();
+}
+
+//Parse topics into file picker
+function ParseInTopics(modalName) {
+  document.getElementById(modalName + "-HierachyLevel-Name").innerHTML = "Topics"
+  document.getElementById(modalName + "-List").innerHTML = "";
+  document.getElementById(modalName + "-HierachyLevel-Back").style.visibility = "hidden";
+  
+  for (i = 0; i < noteList.length; i++) {
+    var topic = document.createElement("div");
+    topic.setAttribute("class", "FilePicker-Item");
+    topic.innerHTML = noteList[i].topic;
+    topic.setAttribute("onclick", "ParseInNotebooks('" + modalName + "', " + i + ");")
+
+    document.getElementById(modalName + "-List").innerHTML = document.getElementById(modalName + "-List").innerHTML + topic.outerHTML;
+  }
+}
+
+//Parse notebooks into file picker
+function ParseInNotebooks(modalName, topicIndex) {
+  document.getElementById(modalName + "-List").innerHTML = "";
+  document.getElementById(modalName + "-HierachyLevel-Name").innerHTML = noteList[topicIndex].topic;
+  document.getElementById(modalName + "-HierachyLevel-Back").style.visibility = "visible";
+  document.getElementById(modalName + "-HierachyLevel-Back").setAttribute("href", "javascript:ParseInTopics('" + modalName + "');");
+
+  for (i = 0; i < noteList[topicIndex].notebooks.length; i++) {
+    var topic = document.createElement("div");
+    topic.setAttribute("class", "FilePicker-Item");
+    topic.innerHTML = noteList[topicIndex].notebooks[i].name;
+    topic.setAttribute("onclick", "ParseInNotes('" + modalName + "', " + topicIndex + "," + i + ");");
+
+    document.getElementById(modalName + "-List").innerHTML = document.getElementById(modalName + "-List").innerHTML + topic.outerHTML;
+  }
+}
+
+//Parse notes into file picker
+function ParseInNotes(modalName, topicIndex, notebookIndex) {
+  document.getElementById(modalName + "-List").innerHTML = "";
+  document.getElementById(modalName + "-HierachyLevel-Name").innerHTML = noteList[topicIndex].notebooks[notebookIndex].name;
+  document.getElementById(modalName + "-HierachyLevel-Back").setAttribute("href", "javascript:ParseInNotebooks('" + modalName + "', " + topicIndex + ");");
+
+  globalTopicIndex = topicIndex; globalNotebookIndex = notebookIndex;
+
+  for (i = 0; i < noteList[topicIndex].notebooks[notebookIndex].notes.length; i++) {
+    var topic = document.createElement("div");
+    topic.setAttribute("class", "FilePicker-Item");
+    topic.innerHTML = noteList[topicIndex].notebooks[notebookIndex].notes[i].name;
+    topic.setAttribute("onclick", "ChangeNoteNameTo('" + modalName + "', this);");
+
+    document.getElementById(modalName + "-List").innerHTML = document.getElementById(modalName + "-List").innerHTML + topic.outerHTML;
+  }
+}
+
+//Switch to private note
+function PrivateNote() {
+  if (CheckSignIn() == true) {
+    privateNote = true;
+    var username = atob(localStorage.getItem("loggedIn")).split(",")[0], password = atob(localStorage.getItem("loggedIn")).split(",")[1];
+
+    var getPHPFile = new XMLHttpRequest();
+    getPHPFile.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("Modal-SaveAdvanced-List").innerHTML = "";
+        console.log(getPHPFile.responseText);
+
+        var privateNotes = JSON.parse(getPHPFile.responseText);
+
+        for (i = 0; i < privateNotes.length; i++){
+          var topic = document.createElement("div");
+          topic.setAttribute("class", "FilePicker-Item");
+          topic.innerHTML = privateNotes[i];
+          topic.setAttribute("onclick", "ChangeNoteNameTo('Modal-SaveAdvanced', this);");
+
+          document.getElementById("Modal-SaveAdvanced-List").innerHTML = document.getElementById("Modal-SaveAdvanced-List").innerHTML + topic.outerHTML;
+        }
+
+        ShowModal("SaveAdvanced");
+      }
+    }
+    getPHPFile.open("POST", "https://notehub-serverside.000webhostapp.com/handlers/filing.php", true);
+    getPHPFile.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    getPHPFile.send("username=" + username + "&password=" + password + "&requestedFunction=GetPrivateNotes");
+  }
+  else {
+    ShowModal("AuthRequired");
+  }
+}
+
+//Switch to public note
+function PublicNote() {
+  privateNote = false;
+  InitFilePicker();
+}
+
+//Set a note as a suggestion
+function ChangeNoteNameTo(modalName, note) {
+  document.getElementById(modalName + "-WillBeSuggestion").style.display = "block";
+  document.getElementById(modalName + "-SaveName").value = note.innerHTML;
+  isSuggestion = true;
+}
+
+//Check note name
+function CheckNoteName(modalName, noteName) {
+  for (i = 0; i < noteList[globalTopicIndex].notebooks[globalNotebookIndex].notes.length; i++) {
+    if (noteName == noteList[globalTopicIndex].notebooks[globalNotebookIndex].notes[i].name) { document.getElementById(modalName + "-WillBeSuggestion").style.display = "block"; }
+    else { document.getElementById(modalName + "-WillBeSuggestion").style.display = "none"; isSuggestion = false; }
+  }
+}
+
+//Save Note As
+function SaveNoteAs(fromMenu) {
+  if (fromMenu == true) {
+    InitFilePicker();
+  }
+  else {
+
+  }
 }
 
 //Save Note
+function SaveNote() {
+  if (noteOpened == true) {
 
-//Show tooltip explaining note size restrictions
-function SizeMatters() {
-  ShowTooltip("SizeMatters");
+  }
+  else {
+    SaveNoteAs();
+  }
 }
 
-//Show report pane
-function ShowReportPane() {
-  ShowPane("NoteSavingReport");
+//Save Note
+function SaveNote() {
+
 }
 
 //Decode html entities
@@ -55,28 +155,7 @@ function decodeHTML(html) {
 
 //Retrieve Note
 function GetNote(topic, noteID) {
-  var getPHPFile = new XMLHttpRequest();
-  getPHPFile.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      //Parse Note
-      console.log(getPHPFile.responseText);
-      currentNoteAsObject = JSON.parse(getPHPFile.responseText);
-      
-      //Debug
-      console.log("Note requested and response was received.");
-      console.log(currentNoteAsObject);
-      
-      if ((currentNoteAsObject.meta.notebook = "NoteHub Basics") || (currentNoteAsObject.meta.notebook = "My Private Notebook")) {
-        privateNote = true;
-      }
-      
-      //Note Editor
-      document.getElementById("Title-Title").value = currentNoteAsObject.meta.name;
-      document.getElementById("Editor").innerHTML = currentNoteAsObject.content;
-    }
-  }
-  getPHPFile.open("GET", "https://notehub-serverside.000webhostapp.com/handlers/filing.php?requestedFunction=read&topic=" + topic + "&noteId=" + noteID, true);
-  getPHPFile.send();
+  
 }
 
 //Copy Note to Editor
