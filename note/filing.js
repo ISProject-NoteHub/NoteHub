30b8cc00b1bf627b6e94fda9fff2e583a34997ef
TheoLeeCJ <theodorelcj@gmail.com>
2017-06-12 10:56:55 +0800

    Updated Filing for Note Editor to use Quill

--- note/filing.js
+++ note/filing.js
@@ -151,7 +151,7 @@ function SaveNoteAs(fromMenu) {
     var note = {
       author: atob(localStorage.getItem("loggedIn")).split(",")[0],
       suggestions: [],
-      content: document.getElementById("Editor").innerHTML.trim().replace("&nbsp;", " ")
+      content: document.getElementsByClassName('ql-editor')[0].innerHTML.trim().replace("&nbsp;", " ")
     };
 
     ShowModal("SavingNote");
@@ -191,7 +191,7 @@ function SaveNote() {
     var note = {
       author: atob(localStorage.getItem("loggedIn")).split(",")[0],
       suggestions: [],
-      content: document.getElementById("Editor").innerHTML.trim().replace("&nbsp;", " ")
+      content: document.getElementsByClassName('ql-editor')[0].innerHTML.trim().replace("&nbsp;", " ")
     };
 
     ShowModal("SavingNote");
