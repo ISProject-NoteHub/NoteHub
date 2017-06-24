49f8b4258d0139e318642da11c19d133489ff830
TheoLeeCJ <theodorelcj@gmail.com>
2017-06-24 16:49:18 +0800

    New Note Editor

---
+++ note/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!--HTML document begins here-->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>
+    <?php echo "New Note"; ?>
+    &nbsp;| NoteHub
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link href="/logo-small.png" rel="icon" />
+  <link href="/resources/stylesheets/w3.css" rel="stylesheet" />
+  <link href="/resources/stylesheets/editor-styling.css" rel="stylesheet" />
+
+  <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+
+  <script src="https://use.fontawesome.com/3e1c5661b6.js"></script>
+</head>
+
+<body>
+  New Note
+</body>
+
+</html>
