24f4a9e75f92905a6eb54bc08c66c0a742f5000e
Aloysius Lee <aloyjust1000@gmail.com>
2017-06-25 16:49:15 +0800

    Update and rename index.html to index.php

---
+++ help/index.php
@@ -0,0 +1,72 @@
+<!--HTML document begins here-->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Help | NoteHub</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link href="/logo-small.png" rel="icon" />
+  <link href="/resources/stylesheets/float-labels.css" rel="stylesheet" />
+  <link href="/resources/stylesheets/w3.css" rel="stylesheet" />
+  <link href="/resources/stylesheets/editor-styling.css" rel="stylesheet" />
+
+  <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+
+  <script src="https://use.fontawesome.com/3e1c5661b6.js"></script>
+</head>
+  
+<body>
+  <div class="w3-sidebar w3-bar-block w3-collapse w3-card-2 w3-animate-left" style="width:200px;" id="App-Menu">
+    <button class="w3-bar-item w3-button w3-large w3-hide-large" onclick="CloseAppMenu();">Close &times;</button>
+
+    <img src="/logo.png" width="200" height="200" />
+
+    <div class="w3-container w3-blue">
+      <h5>
+        <i class="fa fa-user-circle-o" aria-hidden="true"></i>&nbsp;&nbsp;
+        Welcome, <?php
+          if (isset($_COOKIE["signedIn"])) { echo explode(",", base64_decode($_COOKIE["signedIn"]))[0]; }
+          else { echo "User"; }
+        ?>!
+      </h5>
+    </div>
+    <a class="active" href="notehub.tk/help/index.html">Home</a>
+      <br><br><a href="notehub.tk/help/getting-started.html">Getting Started</a>
+      <br><br><a href="notehub.tk/help/creating-note.html">Creating a Note</a>
+      <br><br><a href="notehub.tk/help/account-management.html">Account Management</a>
+    <hr>
+    <?php
+      if (isset($_COOKIE["signedIn"])) { echo '<a href="../accounts/sign-out.php" class="w3-bar-item w3-button"><i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;&nbsp;Sign Out</a>'; }
+      else { echo '<a href="../accounts/sign-in.php" class="w3-bar-item w3-button"><i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;&nbsp;Sign In</a>'; }
+    ?>
+  </div>
+
+  <div class="w3-main" style="margin-left: 200px">
+    <div class="w3-container" id="App-TitleBar">
+      <button class="w3-button w3-xlarge w3-hide-large" onclick="OpenAppMenu();">&#9776;</button>
+
+      <a href="/" style="color: white; text-decoration: none; font-size: 24px;">
+        <span style="font-weight: lighter;">Note</span><b>Hub</b>
+      </a>
+      <span id="Page-Name"> | Help</span>
+    </div>
+
+    <div class="w3-container">
+      <b style="font-size: 20px">Welcome to the Help Page!</b>
+    <hr>
+    Feeling lost? No problem! Pay us a quick visit at the help page!
+    If your questions have no answers in the following pages, please contact us at <a href="/cdn-cgi/l/email-protection#63170b064d0d0c17060b160123040e020a0f4d000c0e"><span class="__cf_email__" data-cfemail="f7839f92d9999883929f8295b7909a969e9bd994989a">[email&#160;protected]</span><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script></a>.
+   <br><br>
+    </div>
+  </div>
+
+  <script>
+    function OpenAppMenu() {
+      document.getElementById("App-Menu").style.display = "block";
+    }
+    function CloseAppMenu() {
+      document.getElementById("App-Menu").style.display = "none";
+    }
+  </script>
+</body>
+</html>
