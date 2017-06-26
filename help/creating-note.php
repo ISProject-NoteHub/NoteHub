1de18042e4d166dd8b1293ea2b034472f37d3870
Aloysius Lee <aloyjust1000@gmail.com>
2017-06-26 19:04:17 +0800

    Update creating-note.php

--- help/creating-note.php
+++ help/creating-note.php
@@ -39,8 +39,9 @@
       </h5>
     </div>
       <a href="/help/index.php">Help Main Page</a>
-      <br><br> <a href="/help/getting-started.php">Getting Started</a>
-      <br><br><a class="active" href="/help/creating-note.php">Creating a Note</a>
+      <br><br><a href="/help/getting-started.php">Getting Started</a>
+      <br><br><a href="/help/creating-note.php">Creating a Note</a>
+      <br><br><a href="/help/gallery">Navigating the Gallery</a>
       <br><br><a href="/help/account-management.php">Account Management</a>
     <hr>
     <?php
