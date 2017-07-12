0ab0b9f3db68d78340c2a39d9290bb83464e4b3a
TheoLeeCJ <theodorelcj@gmail.com>
2017-07-13 05:48:38 +0800

    ...

--- note/backend/public-notes.php
+++ note/backend/public-notes.php
@@ -6,7 +6,7 @@
   function WriteNote() {
     if ($_POST["saveas"] == "true") {
       //Write to notes map
-      $notesMap = json_decode(file_get_contents("../../databases/notes/notes-map.json"));
+      //$notesMap = json_decode(file_get_contents("../../databases/notes/notes-map.json"));
 
       //Write to note
       file_put_contents("../../databases/notes/" . $_POST["topic"] . "/" . $_POST["notename"] . ".txt", $_POST["note"]);
