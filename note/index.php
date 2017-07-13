0421be563288f119ba72cd51d763201254bec1ee
TheoLeeCJ <theodorelcj@gmail.com>
2017-07-13 08:53:36 +0800

    QUick

--- note/index.php
+++ note/index.php
@@ -171,7 +171,7 @@
       <!--Equivalent of the old #Editor-->
       <div id="Editor"><?php
         if (isset($_GET["note"])) {
-          $a = json_decode(file_get_contents("../databases/notes/" . explode("/", $_GET["note"])[0] . "/" . explode("/", $_GET["note"])[1] . ".txt"));
+          $a = file_get_contents("../databases/notes/" . explode("/", $_GET["note"])[0] . "/" . explode("/", $_GET["note"])[1] . ".txt");
           echo $a;
         }
       ?></div>
