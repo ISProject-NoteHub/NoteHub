aea3ea8b7a0b454a558928f12830334f334e9746
TheoLeeCJ <theodorelcj@gmail.com>
2017-08-10 07:40:52 +0800

    Private Notes can now be saved

--- databases/notes/private-notes/list.php
+++
@@ -1,3 +0,0 @@
-<?php
-  function ListPrivateNotes($user) { return json_encode(scandir("../databases/notes/private-notes/" . $user)); }
-?>
---
+++ note/backend/private-notes.php
@@ -0,0 +1,37 @@
+<?php
+  $path = $_SERVER['DOCUMENT_ROOT'];
+  $path .= "/accounts/verify-account.php";
+  include($path);
+
+  function ListPrivateNotes($username, $password) {
+    if (VerifyAccount($username, $password)) {
+      $notes = scandir("../databases/notes/private-notes/" . $username);
+      $notesOutput = array();
+      
+      for ($i = 2; $i < count($notes); $i++) {
+        $notesOutput[$i - 2] = str_replace(".txt", "", $notes[$i]);
+      }
+
+      return $notesOutput;
+    }
+    else {
+      return false;
+    }
+  }
+
+  function WritePrivateNote($username, $password, $noteName, $noteContent) {
+    include("../databases/microdb/Database.php");
+    include("../databases/microdb/Cache.php");
+    include("../databases/microdb/Event.php");
+    include("../databases/microdb/Index.php");
+
+    if (VerifyAccount(explode(',', base64_decode($_COOKIE['signedIn']))[0], explode(',', base64_decode($_COOKIE['signedIn']))[1])) {
+      $write = file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/databases/notes/private-notes/" . $username . "/" . $noteName . ".txt", $noteContent);
+      if ($write !== false) { echo "Write successful."; }
+      else { echo "Write failed."; }
+    }
+    else {
+      echo "Invalid account.";
+    }
+  }
+?>
