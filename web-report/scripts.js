f0175214e01b75fffd394c411ac4b326614952ae
HoWingYip <sploooosh884@gmail.com>
2017-08-10 10:59:45 +0800

    Web Report - Commit 1. Done for now, until Aloysius adds more to the Google Doc.

--- web-report/scripts.js
+++ web-report/scripts.js
@@ -6,22 +6,21 @@ function hideLogo() {
   $('.report-logo').fadeOut();
   $('.report-logo').css("display","none");
 }
-
 $(window).on('scroll', function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 200) {
-      $('.top').stop().animate({
-        top: "-20px",
-        height: "70px",
-        fontSize: "20px",
-      },200);
-      hideLogo();
-    } else {
-      $('.top').stop().animate({
-        top: "0",
-        height: "100vh",
-        fontSize: "50px",
-      },200);
-      showLogo();
-    }
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop > 200) {
+    $('.top').stop().animate({
+      top: "-20px",
+      height: "70px",
+      fontSize: "20px",
+    },200);
+    hideLogo();
+  } else {
+    $('.top').stop().animate({
+      top: "0",
+      height: "100vh",
+      fontSize: "50px",
+    },200);
+    showLogo();
+  }
 });
